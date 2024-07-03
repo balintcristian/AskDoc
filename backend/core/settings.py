d60a10ad5e333593a5bbcb16c6b9fc22ce8c7226
balintcristian <cristi.balint11@gmail.com>
2024-07-04 00:16:02 +0300

    quick commit
    removed unnecessary static file and modified .main to not use react restricted mode

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,8 +24,8 @@ INSTALLED_APPS = [
     'queries',
 
 ]
-CORS_ALLOWED_ORIGINS=['http://localhost:5173']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS=['http://localhost:5173','http://localhost:4173']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:5173','http://localhost:4173']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
